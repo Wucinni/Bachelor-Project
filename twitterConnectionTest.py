b10b67a13784edeac9009afe881789210511d258
Your Name <you@example.com>
2023-03-12 18:38:19 +0200

    first api search module

--- twitterConnectionTest.py
+++ twitterConnectionTest.py
@@ -1,1 +1,34 @@
 #Github commit test
+
+import requests
+import os
+
+import requests
+import json
+#its bad practice to place your bearer token directly into the script (this is just done for illustration purposes)
+BEARER_TOKEN = "AAAAAAAAAAAAAAAAAAAAAOqPkAEAAAAA%2FjppBJBHiGf3cyds4poEBU956lk%3DypGk85lgeQUOM9ifgWweAI1ul6RF0az1hRkQX40NYxKB7c7HW8"
+#define search twitter function
+def search_twitter(query, tweet_fields, bearer_token = BEARER_TOKEN):
+    headers = {"Authorization": "Bearer {}".format(bearer_token)}
+
+    url = "https://api.twitter.com/2/tweets/search/recent?query={}&{}".format(
+        query, tweet_fields
+    )
+    response = requests.request("GET", url, headers=headers)
+
+    print(response.status_code)
+
+    if response.status_code != 200:
+        raise Exception(response.status_code, response.text)
+    return response.json()
+
+#Function call
+#search term
+query = "emag"
+#twitter fields to be returned by api call
+tweet_fields = "tweet.fields=text,author_id,created_at"
+
+#twitter api call
+json_response = search_twitter(query=query, tweet_fields=tweet_fields, bearer_token=BEARER_TOKEN)
+#pretty printing
+print(json.dumps(json_response, indent=4, sort_keys=True))
